3a2fd0fca4b99bf6e26e121bfc3f7050453ec7bb
Maximiliano Osorio <maxiosorio@gmail.com>
2023-03-23 12:05:52 -0300

    fix: reorder

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from provDocumentWrapper import ProvDocumentWrapper
-from narrative import DataNarrative
-from templateHandler import render_template
+import argparse
+from src.provDocumentWrapper import ProvDocumentWrapper
+from src.narrative import DataNarrative
+
+
+# Parse argument from command line
+# -i {path_to_prov_document}
+# -o {path_to_output_file}
+# -h {hypothesis_name}
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Path to input file", required=True)
+    parser.add_argument("-n", "--hypothesis", help="Hypothesis name", required=True)
+    parser.add_argument("-o", "--output", help="Path to output file")
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == '__main__':
-    hypothesisName = 'Hypothesis-4CGdVLyttD07'
-    doc_path = "../DISK-OPMW-Mapper/examples/document.json"
+    arguments = parse_args()
+    doc_path = arguments.input
+    hypothesisName = arguments.hypothesis
     provDocumentWrapper = ProvDocumentWrapper(doc_path)
     dataNarrative = DataNarrative(provDocumentWrapper, hypothesisName)
     print(dataNarrative.text)
-    # provDocumentWrapper.draw()
